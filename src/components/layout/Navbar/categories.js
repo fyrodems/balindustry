ec2db68cd0e9e3f46a25d3f6eab85d55ef50cdd3
Martyna Filipiak <fyrodems@gmail.com>
2024-03-13 17:42:34 +0100

    backup for Navbar (with mapped categories)

---
+++ src/components/layout/Navbar/categories.js
@@ -0,0 +1,106 @@
+export const categories = [
+  {
+    name: 'Robotyzacja i automatyzacja',
+    subcategories: [
+      {
+        name: 'Stanowisko zrobotyzowane',
+        items: [
+          { name: 'Stanowisko Single', image: '/navbarProducts/single.svg' },
+          { name: 'Stanowisko Dual', image: '/navbarProducts/placeholder.svg' },
+        ],
+      },
+      {
+        name: 'Automatyzacja maszyn i urządzeń',
+        items: [
+          {
+            name: 'Automatyzacja maszyn',
+            image: '/navbarProducts/automatyzacja.svg',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Park maszynowy',
+    subcategories: [
+      {
+        name: 'Dział obróbki metalowej',
+        items: [
+          { name: 'Waterjet' },
+          { name: 'TruPunch 5000 | TRUMPF' },
+          { name: 'TrueLaser Tube 7000 fiber' },
+          { name: 'TrueLaser 3030 fiber | TRUMPF' },
+          { name: 'Stanowisko gięcia' },
+          { name: 'ARC D600' },
+          { name: 'ARC B250' },
+          { name: 'ARC Trackmotion 2t' },
+        ],
+      },
+      {
+        name: 'Dział obróbki CNC',
+        items: [
+          { name: 'CLX 350' },
+          { name: 'DMU 75 monoBLOCK' },
+          { name: 'DMU 210 P' },
+          { name: 'SPRINT 3215' },
+          { name: 'M1' },
+          { name: 'Waterjet' },
+          { name: 'LH' },
+          { name: '5 osiowe CNC' },
+          { name: 'NLX 2500' },
+        ],
+      },
+      {
+        name: 'Lakiernie',
+        items: [{ name: 'Lakiernie mokre i proszkowe' }],
+      },
+    ],
+  },
+  {
+    name: 'Produkty',
+    subcategories: [
+      {
+        name: '',
+        items: [
+          {
+            name: 'Obróbka powierzchni',
+            image: '/navbarProducts/placeholder.svg',
+          },
+        ],
+      },
+      {
+        name: '',
+        items: [
+          {
+            name: 'Magazyn pionowy blach',
+            image: '/navbarProducts/placeholder.svg',
+          },
+        ],
+      },
+      {
+        name: '',
+        items: [
+          { name: 'Piece komorowe', image: '/navbarProducts/placeholder.svg' },
+        ],
+      },
+      {
+        name: '',
+        items: [
+          { name: 'Lakiernie', image: '/navbarProducts/placeholder.svg' },
+        ],
+      },
+      {
+        name: 'Spawalnictwo5',
+        items: [
+          { name: 'ALW-1200' },
+          { name: 'Flex Cobot Esab' },
+          { name: 'Flex Cobot Fronius' },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Usługi',
+    subcategories: [],
+  },
+]
