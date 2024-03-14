ab5a525e544fec3bb9a144b52a43d240de6b6152
Martyna Filipiak <fyrodems@gmail.com>
2024-03-14 10:56:01 +0100

    zmiana contentu w navbarze

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -8,10 +8,11 @@ import {
   MenubarMenu,
   MenubarTrigger,
 } from '@/components/ui/menubar'
+import { SubCategoryLabel } from './SubCategoryLabel'
 
 const Navbar = () => {
   return (
-    <div className="@apply top-0; bg-navbar sticky z-[2] flex min-h-[70px] w-full items-center justify-between px-[38px] py-0 text-white transition-all ">
+    <div className="@apply top-0; sticky z-[2] flex min-h-[70px] w-full items-center justify-between bg-navbar px-[38px] py-0 text-white transition-all ">
       <Image
         src={'/logos/BALIndustryLogoWhite.svg'}
         alt={'logo'}
@@ -33,44 +34,44 @@ const Navbar = () => {
               <Image
                 src={'/navbarProducts/single.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
               <div>
-                <span>Stanowisko zrobotyzowane</span>
+                <SubCategoryLabel content={'Stanowisko zrobotyzowane'} />
                 <span>Stanowisko Single</span>
               </div>
             </MenubarItem>
             <MenubarItem>
               <Image
-                src={'/navbarProducts/dual.svg'}
+                src={'/navbarProducts/placeholder.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
               <div>
-                <span>Stanowisko zrobotyzowane</span>
+                <SubCategoryLabel content={'Stanowisko zrobotyzowane'} />
                 <span>Stanowisko Single</span>
               </div>
             </MenubarItem>
             <MenubarItem>
               <Image
-                src={'/navbarProducts/dual.svg'}
+                src={'/navbarProducts/placeholder.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
               <div>
-                <span>Stanowisko zrobotyzowane</span>
-                <span>Stanowisko Single</span>
+                <SubCategoryLabel content={'Stanowisko zrobotyzowane'} />
+                <span>Stanowisko Twin One-axis</span>
               </div>
             </MenubarItem>
             <MenubarItem>
               <Image
                 src={'/navbarProducts/automatyzacja.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
 
               <span>Automatyzacja maszyn i urządzeń</span>
@@ -83,7 +84,7 @@ const Navbar = () => {
 
           <MenubarContent>
             <div>
-              <span>Dział obróbki metalowej</span>
+              <SubCategoryLabel content={'Dział obróbki metalowej'} />
               <MenubarItem>Waterjet</MenubarItem>
               <MenubarItem>TruPunch 5000 | TRUMPF</MenubarItem>
               <MenubarItem>TrueLaser Tube 7000 fiber</MenubarItem>
@@ -94,7 +95,7 @@ const Navbar = () => {
               <MenubarItem>ARC Trackmotion 2t</MenubarItem>
             </div>
             <div>
-              <span>Dział obróbki CNC</span>
+              <SubCategoryLabel content={'Dział obróbki CNC'} />
               <MenubarItem>CLX 350</MenubarItem>
               <MenubarItem>DMU 75 monoBLOCK</MenubarItem>
               <MenubarItem>DMU 210 P</MenubarItem>
@@ -106,7 +107,7 @@ const Navbar = () => {
               <MenubarItem>NLX 2500</MenubarItem>
             </div>
             <div>
-              <span>Lakiernie</span>
+              <SubCategoryLabel content={'Lakiernie'} />
               <MenubarItem>Lakiernie mokre i proszkowe</MenubarItem>
             </div>
           </MenubarContent>
@@ -117,46 +118,46 @@ const Navbar = () => {
           <MenubarContent>
             <MenubarItem>
               <Image
-                src={'/navbarProducts/dual.svg'}
+                src={'/navbarProducts/placeholder.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
 
               <span>Obróbka powierzchni</span>
             </MenubarItem>
             <MenubarItem>
               <Image
-                src={'/navbarProducts/dual.svg'}
+                src={'/navbarProducts/placeholder.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
 
               <span>Magazyn pionowy blach</span>
             </MenubarItem>
             <MenubarItem>
               <Image
-                src={'/navbarProducts/dual.svg'}
+                src={'/navbarProducts/placeholder.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
 
               <span>Piece komorowe</span>
             </MenubarItem>
             <MenubarItem>
               <Image
-                src={'/navbarProducts/dual.svg'}
+                src={'/navbarProducts/placeholder.svg'}
                 alt={'single'}
-                width={79}
-                height={79}
+                width={80}
+                height={80}
               />
 
               <span>Lakiernie</span>
             </MenubarItem>
             <div>
-              <span>Dział obróbki CNC</span>
+              <SubCategoryLabel content={'Spawalnictwo'} />
               <MenubarItem>ALW-1200</MenubarItem>
               <MenubarItem>Flex Cobot Esab</MenubarItem>
               <MenubarItem>Flex Cobot Fronius</MenubarItem>
@@ -171,7 +172,7 @@ const Navbar = () => {
 
       <Menubar>
         <Link
-          className="bg-main-orange cursor-pointer select-none items-center rounded-sm px-5 py-1.5 text-sm transition-all hover:bg-orange-400"
+          className="cursor-pointer select-none items-center rounded-sm bg-main-orange px-5 py-1.5 text-sm transition-all hover:bg-orange-400"
           href={''}
         >
           Kontakt
