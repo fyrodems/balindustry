155f68e61e08ddb4e744bff076dff2e600700bbc
fyrodems <fyrodems@gmail.com>
2024-03-16 01:59:54 +0100

    advantage section with  styles

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -6,20 +6,22 @@ import Partners from '@/components/homePage/Partners/Partners'
 
 const About = () => {
   return (
-    <main>
-      <PageTitle content="O firmie" />
-      <p className="mx-auto mb-[80px] max-w-[1100px] px-[20px] text-center">
-        Nasza firma istnieje na rynku od 1998 roku. Szybko rozwijaliśmy się jako
-        zakład produkujący wiele produktów o różnym przeznaczeniu. Wraz z
-        wzrostem zapotrzebowania na rynku na oferowane przez naszą firmę
-        produkty inwestowaliśmy w rozwój parku maszynowego, celem zwiększenia
-        wydajności oraz jakości naszych produktów.
-      </p>
+    <>
+      <main>
+        <PageTitle content="O firmie" />
+        <p className="mx-auto mb-[80px] max-w-[1100px] px-[20px] text-center">
+          Nasza firma istnieje na rynku od 1998 roku. Szybko rozwijaliśmy się
+          jako zakład produkujący wiele produktów o różnym przeznaczeniu. Wraz z
+          wzrostem zapotrzebowania na rynku na oferowane przez naszą firmę
+          produkty inwestowaliśmy w rozwój parku maszynowego, celem zwiększenia
+          wydajności oraz jakości naszych produktów.
+        </p>
+        <Partners />
+        <AboutBalIndustry />
+      </main>
       <CompanyAdvantages />
-      <Partners />
-      <AboutBalIndustry />
       <Statistics />
-    </main>
+    </>
   )
 }
 
--- src/components/about/CompanyAdvantages/CompanyAdvantages.tsx
+++ src/components/about/CompanyAdvantages/CompanyAdvantages.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 const Advantage = ({ icon, title, content }) => {
   return (
-    <div>
+    <div className="flex w-[330px] flex-col gap-[16px] text-white">
       <Image src={icon} alt={title} width={70} height={70} />
-      <h3>{title}</h3>
+      <h3 className="text-2xl font-bold">{title}</h3>
       <p>{content}</p>
     </div>
   )
@@ -12,32 +12,34 @@ const Advantage = ({ icon, title, content }) => {
 
 const CompanyAdvantages = () => {
   return (
-    <div className="inline-flex w-full items-center justify-start gap-2.5 bg-stone-800 p-[150px]">
-      <Advantage
-        icon={'/advantages/advantage1.svg'}
-        title="Kreatywność"
-        content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
-      />
-      <Advantage
-        icon={'/advantages/advantage2.svg'}
-        title="Praca zespołowa"
-        content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
-      />
-      <Advantage
-        icon={'/advantages/advantage3.svg'}
-        title="Pomysłowość"
-        content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
-      />
-      <Advantage
-        icon={'/advantages/advantage4.svg'}
-        title="Jakość"
-        content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
-      />
-      <Advantage
-        icon={'/advantages/advantage5.svg'}
-        title="Nowoczesne zastosowania"
-        content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
-      />
+    <div className="place-center grid bg-stone-800 px-[30px] py-[150px]">
+      <div className="mx-auto inline-flex w-full max-w-[1300px] flex-wrap items-center justify-start gap-24">
+        <Advantage
+          icon={'/advantages/advantage1.svg'}
+          title="Kreatywność"
+          content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
+        />
+        <Advantage
+          icon={'/advantages/advantage2.svg'}
+          title="Praca zespołowa"
+          content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
+        />
+        <Advantage
+          icon={'/advantages/advantage3.svg'}
+          title="Pomysłowość"
+          content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
+        />
+        <Advantage
+          icon={'/advantages/advantage4.svg'}
+          title="Jakość"
+          content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
+        />
+        <Advantage
+          icon={'/advantages/advantage5.svg'}
+          title="Nowoczesne zastosowania"
+          content="High-end digital experiences. Created  the heart Manhattan, we are a should human team driving force."
+        />
+      </div>
     </div>
   )
 }
