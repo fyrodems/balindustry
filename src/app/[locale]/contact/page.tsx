c0ca50cac4139af48a056a72a614ab92a0c3c0f3
fyrodems <fyrodems@gmail.com>
2024-03-17 22:15:32 +0100

    add Contact page

---
+++ src/app/[locale]/contact/page.tsx
@@ -0,0 +1,13 @@
+import PageTitle from '@/components/common/PageTitle'
+
+const Contact = () => {
+  return (
+    <>
+      <main>
+        <PageTitle content="Formularz kontaktowy" />
+      </main>
+    </>
+  )
+}
+
+export default Contact
