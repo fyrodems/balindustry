978ffc65bd45fb5964af6cd59a5eb34e9978ead8
fyrodems <fyrodems@gmail.com>
2024-03-18 22:48:34 +0100

    add intro to contact form

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -1,10 +1,21 @@
 import PageTitle from '@/components/common/PageTitle'
+import Intro from '@/components/contact/Intro/Intro'
+import styles from './page.module.scss'
 
 const Contact = () => {
   return (
     <>
       <main>
         <PageTitle content="Formularz kontaktowy" />
+        <p className="mx-auto mb-[60px] max-w-[1100px] text-balance px-[20px] text-center">
+          Dziękujemy za zainteresowanie naszą firmą! Prosimy o wypełnienie
+          poniższego formularza, aby skontaktować się z nami. Postaramy się
+          odpowiedzieć jak najszybciej.
+        </p>
+        <div className={styles.formWrapper}>
+          <Intro />
+          <div></div>
+        </div>
       </main>
     </>
   )
---
+++ src/components/contact/Intro/Intro.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+
+const DirectContact = ({ icon, title, content }) => {
+  return (
+    <div className="flex inline-flex h-[90px] w-[340px] items-center justify-between gap-2.5 rounded-[180px] bg-orange-500 px-10 py-5 my-4">
+      <Image src={icon} alt={title} width={40} height={30} />
+      <div className="flex flex-col text-white">
+        <span className="min-w-[192px] font-bold">{title}</span>
+        <span>{content}</span>
+      </div>
+    </div>
+  )
+}
+
+const Intro = () => {
+  return (
+    <div className='p-[30px]'>
+      <div>
+        <h3 className="text-[36px] font-bold text-gray-950">
+          Skontaktuj się z nami
+        </h3>
+        <span className='block max-w-[335px] my-4'>
+          Prześlij nam e-mail lub rozpocznij rozmowę, wypełniając poniższy
+          formularz.
+        </span>
+      </div>
+
+      <div className="flex flex-col">
+        <h3 className="text-[28px] font-bold text-gray-950">
+          Skontaktuj się z nami bezpośrednio!
+        </h3>
+        <DirectContact
+          icon="/contact/mail.svg"
+          title={'Email'}
+          content={'kontakt@balindustry.com'}
+        />
+        <DirectContact
+          icon="/contact/phone.svg"
+          title={'Telefon'}
+          content={'123 123 123'}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Intro
