e7dae6cac02f6881aabe2e0ded4852d4f05026d9
Martyna Filipiak <fyrodems@gmail.com>
2024-03-21 15:00:48 +0100

    finished DMU 210 P

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
-    <footer className="bg-footer grid place-items-center bg-navbar p-6 text-slate-200">
-      Copyright © 2024 balindustry.com. All rights reserved
+    <footer className="bg-footer laptop:flex-row flex flex-col justify-center text-balance bg-navbar p-6 text-center text-slate-200">
+      <span>Copyright © 2024 balindustry.com.&nbsp;</span>
+      <span>All rights reserved</span>
     </footer>
   )
 }
