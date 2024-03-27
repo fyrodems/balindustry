dfe38f9e8481660931cb096ea68a79a5641056a5
Martyna Filipiak <fyrodems@gmail.com>
2024-03-27 10:47:44 +0100

    floating cta with 3d

--- src/components/product/FloatingCTA/FloatingCTA.tsx
+++ src/components/product/FloatingCTA/FloatingCTA.tsx
@@ -6,7 +6,7 @@ import { Link } from '@/navigation'
 import { initializeArButton } from '@/utils/createQR'
 
 const FloatingCTA: React.FC<FloatingCTAProps> = ({ data }) => {
-  const { title, image, content, button, form, qr, ar } = data
+  const { title, image, content, button, form, qr, ar, arLink } = data
 
   /**
    * !!! do uzupełnienia
@@ -15,10 +15,10 @@ const FloatingCTA: React.FC<FloatingCTAProps> = ({ data }) => {
    *
    */
 
-  useEffect(() => {
-    const arLink = `${window.location.protocol}//${window.location.host}/${ar}`
-    initializeArButton(arLink)
-  }, [])
+  // useEffect(() => {
+  //   const arLink = `${window.location.protocol}//${window.location.host}/${ar}`
+  //   initializeArButton(arLink)
+  // }, [])
 
   return (
     <div
@@ -26,22 +26,33 @@ const FloatingCTA: React.FC<FloatingCTAProps> = ({ data }) => {
       className="fixed right-[38px] top-[150px] rounded border border-zinc-200 p-4 2xl:right-auto 2xl:ml-[40px] 2xl:p-6"
     >
       <span className="text-lg">{title}</span>
-      {ar ? (
+      {/* {ar ? (
         <canvas id="qr-code" className="mx-auto w-[175px]" />
-      ) : (
-        <Image
-          src={image || qr}
-          alt={''}
-          width={200}
-          height={100}
-          className="mx-auto my-6"
-        />
-      )}
+      ) : ( */}
+      <Image
+        src={image || qr}
+        alt={''}
+        width={200}
+        height={100}
+        className="mx-auto my-6"
+      />
+      {/* )} */}
 
       {content && (
         <span className="my-4 block max-w-[220px] whitespace-normal text-center">
           {content}
         </span>
+      )}
+
+      {ar && arLink && (
+        <>
+          <Link href={arLink}>
+            <Button
+              content={'Zobacz w\u00A03D'}
+              className="m-auto my-4 w-[215px] text-wrap"
+            />
+          </Link>
+        </>
       )}
       <Link href={form}>
         <Button content={button} className="m-auto" />
